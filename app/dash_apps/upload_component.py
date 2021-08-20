40cb33aa8df4bcbc2c88319fd9e03433d7c3653c
Lohith Basavaraj <lohith.uvce@gmail.com>
2021-08-20 23:00:19 +0100

    Input read into pd pd -> kdf -> sdf run profiler Results in a datatable

--- app/dash_apps/upload_component.py
+++ app/dash_apps/upload_component.py
@@ -7,13 +7,31 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-
 import pandas as pd
+from utils.spark_utils import get_local_spark_session
+from utils.deutils import run_profile
+from pyspark.sql import functions as f
+import databricks.koalas as ks
+import json
 
+# df = pd.DataFrame()
+
+df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
+                   'num_wings': [2, 0, 0, 0],
+                   'num_specimen_seen': [10, 2, 1, 8]},
+                  index=['falcon', 'dog', 'spider', 'fish']
+                  )
+
+spark = get_local_spark_session(app_name="Upload Application")
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, requests_pathname_prefix="/upload/")
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True,
+    # requests_pathname_prefix="/upload/"
+)
 
 app.layout = html.Div([
     dcc.Upload(
@@ -33,62 +51,93 @@ app.layout = html.Div([
             'margin': '10px'
         },
         # Allow multiple files to be uploaded
-        multiple=True
+        multiple=False
     ),
     html.Div(id='output-data-upload'),
+    html.Div(id='recordCount', children=[]),
+    dcc.Store(id='resultStore', data=[]),
 ])
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
 
 
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
+              State('upload-data', 'last_modified'),
+              prevent_initial_call=True
+              )
+def read_contents_into_pdf(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
+        content_type, content_string = list_of_contents.split(',')
+
+        decoded = base64.b64decode(content_string)
+        global df
+        try:
+            if 'csv' in list_of_names:
+                # Assume that the user uploaded a CSV file
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+            elif 'xls' in list_of_names:
+                # Assume that the user uploaded an excel file
+                df = pd.read_excel(io.BytesIO(decoded))
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+
+    if list_of_contents is None:
+        return html.Div([])
+
+    return html.Div([
+        html.H2("Displaying first 100 records"),
+        html.Br(),
+        dash_table.DataTable(
+            data=df.head(100).to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+            ),
+        html.Br(),
+        html.Button("Start Analysis", id="startAnalysis", n_clicks=0)
+        ])
 
 
+# @app.callback(Output('recordCount', 'children'),
+#               Input('startAnalysis', 'n_clicks'),
+#               )
+# def display_record_count(n_clicks):
+#     record_count = df.count()
+#     if n_clicks>=1:
+#         return html.Div([
+#             html.H3(f"The record count is {record_count}")
+#         ])
+#     else:
+#         return html.Div([])
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+
+@app.callback(Output('recordCount', 'children'),
+              Input('startAnalysis', 'n_clicks'),
+              )
+def display_record_count(n_clicks):
+    if n_clicks>=1:
+        kdf = ks.from_pandas(df)
+        profiled_df = run_profile(spark, kdf.to_spark())
+        unpacked_df = profiled_df.\
+            select(
+            "column_name",
+            f.explode("histogram").alias("histogram")
+        ).selectExpr(
+            "column_name",
+            "histogram.value as value",
+            "histogram.num_occurrences as numOcc",
+            "histogram.ratio as ratio")
+        # data_table = dash_table.DataTable(
+        #     data=unpacked_df.to_dict('records'),
+        #     columns=[{'name': i, 'id': i} for i in df.columns]
+        # )
+        df_as_string = unpacked_df.collect()
+        return html.Div([
+            html.H6(f"The dataframe as a string is {df_as_string}")
+        ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
