a0083f09dc028be85df2c3ca90de8e908eab615a
Lohith Basavaraj <lohith.uvce@gmail.com>
2021-08-29 00:18:37 +0100

    Reverted the koalas read to pandas read

--- utils/spark_utils.py
+++ utils/spark_utils.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 import json
 from pathlib import Path
+from pyspark.sql import DataFrame
+
 
 
 def get_project_root() -> Path:
@@ -36,3 +38,46 @@ def get_spark_conf_as_json(spark: SparkSession) -> json:
     """
     configurations = spark.sparkContext.getConf().getAll()
     return json.dumps(dict(configurations))
+
+
+def transform(self, f):
+    """
+    Method monkey patched into the DataFrame class.
+    It enables to chain the transformations/functions passed as f
+    Usage:  testDF.\
+        transform(with_date_cols_casted).\
+        transform(with_decimal_cols_casted).\
+        transform(trim_columns(["col2", "col2"]))
+    """
+    return f(self)
+
+
+DataFrame.transform = transform
+
+
+def cleanup_col_name(col_name):
+    str_w_space = ' '.join(col_name.split())
+    str_w_underscore = str_w_space.replace(' ', '_' )
+    return (col_name, str_w_underscore.lower())
+
+
+def build_select_expr(cols_map):
+    select_expr = [" `{}` as {}".format(x[0],x[1]) for x in cols_map]
+    return select_expr
+
+
+def with_std_column_names():
+    """
+    Conform all the columns to be stripped of all whitespaces lowercase
+    Args:
+        cols: list of columns
+    Returns:
+        Dataframe with specified columns converted
+    """
+
+    def inner(df):
+        cols_map = [cleanup_col_name(x) for x in df.columns]
+        select_expr=build_select_expr(cols_map)
+        return df.selectExpr(select_expr)
+
+    return inner
